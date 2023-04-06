e5ad23cbe56bda7db65b796291967642be72a7c3
Abhay kumar <60011935+Abhay7879@users.noreply.github.com>
2023-04-06 16:10:40 +0530

    complete

---
+++ lib/contact_screen.dart
@@ -0,0 +1,190 @@
+import 'package:contact_list/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController contactController = TextEditingController();
+  List<Contact> contacts = List.empty(growable: true);
+
+  int selectedIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact List"),
+        automaticallyImplyLeading: false,
+        actions: [
+          TextButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut().then((value) {
+                print("Sign Out Done");
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ));
+              });
+            },
+            child: const Text(
+              "Log Out",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                  hintText: 'Contact Name',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ))),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: contactController,
+              keyboardType: TextInputType.number,
+              maxLength: 10,
+              decoration: const InputDecoration(
+                hintText: 'Contact Number',
+                prefixText: '+91 ',
+                prefixIcon: Icon(Icons.phone),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      String name = nameController.text.trim();
+                      String contact = contactController.text.trim();
+                      if (name.isNotEmpty && contact.isNotEmpty) {
+                        setState(() {
+                          nameController.text = '';
+                          contactController.text = '';
+                          contacts.add(Contact(name: name, contact: contact));
+                        });
+                      }
+                      //
+                    },
+                    child: const Text('Save')),
+                ElevatedButton(
+                    onPressed: () {
+                      //
+                      String name = nameController.text.trim();
+                      String contact = contactController.text.trim();
+                      if (name.isNotEmpty && contact.isNotEmpty) {
+                        setState(() {
+                          nameController.text = '';
+                          contactController.text = '';
+                          contacts[selectedIndex].name = name;
+                          contacts[selectedIndex].contact = contact;
+                          selectedIndex = -1;
+                        });
+                      }
+                      //
+                    },
+                    child: const Text('Update')),
+              ],
+            ),
+            const SizedBox(height: 10),
+            contacts.isEmpty
+                ? const Text(
+                    'No Contact yet..',
+                    style: TextStyle(fontSize: 22),
+                  )
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: contacts.length,
+                      itemBuilder: (context, index) => getRow(index),
+                    ),
+                  )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getRow(int index) {
+    return Card(
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor:
+              index % 2 == 0 ? Colors.deepPurpleAccent : Colors.purple,
+          foregroundColor: Colors.white,
+          child: Text(
+            contacts[index].name[0],
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              contacts[index].name,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(contacts[index].contact),
+          ],
+        ),
+        trailing: SizedBox(
+          width: 70,
+          child: Row(
+            children: [
+              InkWell(
+                  onTap: () {
+                    //
+                    nameController.text = contacts[index].name;
+                    contactController.text = contacts[index].contact;
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                    //
+                  },
+                  child: const Icon(Icons.edit)),
+              InkWell(
+                  onTap: (() {
+                    //
+                    setState(() {
+                      contacts.removeAt(index);
+                    });
+                    //
+                  }),
+                  child: const Icon(Icons.delete)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Contact {
+  String name;
+  String contact;
+  Contact({required this.name, required this.contact});
+}
